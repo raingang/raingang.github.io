45bfeab5c7f6f01ea1313450ab46ef1ef9aa8c19
raingang <raingang4@gmail.com>
2018-03-10 13:35:38 +0200

    Rewrite JS

--- js/common.js
+++ js/common.js
@@ -1,36 +1,35 @@
 $(function() {
-
-	// Пользовательские функции
 	const HEADER_HEIGHT = 70
-	$('#header__burger').click(() =>{
-		$('#main-menu').toggle()
+		$('#header__burger').click(() =>{
+			$('#main-menu').toggle()
 	})
+
+	$(document).click((event) => {
+		let id = event.target.id
+		let target = ''
+		switch(id){
+			case('about-button'):
+				target = 'about-header'
+				break
+			case('tools-button'):
+				target = 'tools-header'
+				break
+			case('contacts-button'):
+				target = 'contacts-header'
+				break
+		}
+		if(target){
+			console.log('from document')
+			$('html, body').animate({
+				scrollTop: ($('#' + target).offset().top - HEADER_HEIGHT)
+			}, 800)
+		}
+	})
+	
 
 	$('#hero__arrow').click(() =>{
 		$('html, body').animate({
 	        scrollTop: ($("#about-header").offset().top - HEADER_HEIGHT)
 	    }, 800);
 	})
-
-	$('#about-button').click(() =>{
-		$('html, body').animate({
-	        scrollTop: ($("#about-header").offset().top - HEADER_HEIGHT)
-	    }, 800);
-	    $('#main-menu').toggle()
-	})
-
-	$('#tools-button').click(() =>{
-		$('html, body').animate({
-	        scrollTop: ($("#tools-header").offset().top - HEADER_HEIGHT)
-	    }, 800);
-	    $('#main-menu').toggle()
-	})
-
-	$('#contacts-button').click(() =>{
-		$('html, body').animate({
-	        scrollTop: ($("#contacts-header").offset().top - HEADER_HEIGHT)
-	    }, 800);
-	    $('#main-menu').toggle()
-	})
-
 });
